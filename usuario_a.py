d8f8748585e709ea6da82df08c03338af3ec9e4c
juanjosegonzalezrosales <juanjosegonzalezrosales@gmail.com>
2026-03-04 23:19:43 -0500

    feat(usuario_a.py): finalizacion de la funcion es_numero_perfecto

--- usuario_a.py
+++ usuario_a.py
@@ -29,4 +29,13 @@ def es_numero_perfecto(numero):
     Recibe un número entero y devuelve True si es perfecto,
     o False si no lo es.
     """
-    return
+    if numero <= 1:
+        return False
+
+    suma_divisores = 0
+
+    for i in range(1, numero):
+        if numero % i == 0:
+            suma_divisores += i
+
+    return suma_divisores == numero
